f9f30583e6b67a224c955d231054476b479942fd
HussainMBH <mbahirh624@gmail.com>
2023-09-21 20:57:00 +0530

    Reverse String

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuffer name = new StringBuffer("Mohamed Bahir ");
+        name = name.append("Hussain");
+
+        String nm = "Bahir";
+
+        System.out.println(name);
+        System.out.println(nm);
+//01-Q given name Agni please write in reverse
+        String given = "Agni";
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(given);
+
+        System.out.println(buffer.reverse());
     }
 }
