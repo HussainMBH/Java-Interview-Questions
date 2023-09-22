8cbcd657a0e59e48b56af9ef5d3957d40c379230
HussainMBH <mbahirh624@gmail.com>
2023-09-22 23:57:47 +0530

    Reverse Pyramid Star Pattern

--- src/Main.java
+++ src/Main.java
@@ -88,5 +88,16 @@ public class Main {
             System.out.println();
         }
 
-    }
+        int ro=8;
+        for (int i= 0; i<= rows-1; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= rows - 1 - i; k++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+        }
 }
