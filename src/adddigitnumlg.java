f08c9866077bb604f3a5a9cd98a8fa88af39f771
HussainMBH <mbahirh624@gmail.com>
2023-10-10 15:59:52 +0530

    Add Digits Leetcode 258

---
+++ src/adddigitnumlg.java
@@ -0,0 +1,18 @@
+//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+public class adddigitnumlg {
+    public static void main(String [] args){
+        adddigitnumlg addgt = new adddigitnumlg();
+        addgt.adddigit();
+    }
+
+    public void adddigit(){
+        int num = 88;
+        if(num < 9){
+            System.out.println(num);
+        }
+        else if (num % 9 == 0){
+            System.out.println(9);
+        }
+        System.out.println(num%9);
+    }
+}
