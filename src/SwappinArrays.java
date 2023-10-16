efffc2e730cc2aec46f0e91f085ea2ebaf42e984
HussainMBH <mbahirh624@gmail.com>
2023-10-16 16:19:03 +0530

    Add Swapping sorting message

--- src/SwappinArrays.java
+++ src/SwappinArrays.java
@@ -4,20 +4,17 @@ public class SwappinArrays {
     public static void main(String[] args){
         int arr[] = {4,3,8,6,5,1};
         int n = arr.length;
-        int tmp = 0;
-        System.out.println(Arrays.toString(arr));
-
-
-        for(int i =0; i<=n-2; i++){
-            int min = i;
-            for (int j=i; j<=n-1; j++){
+        for(int i =0; i<=n-2; i++){ // swapping loop that's why use up to n-2
+            int min = i; //minimum value initialized
+            for(int j = i; j<=n-1; j++){ // finding minimum value that's why use up to n-1
                 if(arr[j] < arr[min]){
                     min = j;
                 }
+                //swap sorting
+                int tmp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = tmp;
             }
-            tmp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = tmp;
         }
 
         for (int i= 0; i<arr.length; i++){
