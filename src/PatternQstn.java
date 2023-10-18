14eb6d67787c02cd8f59a7516cc07e9b7f8676f7
HussainMBH <mbahirh624@gmail.com>
2023-10-18 20:44:56 +0530

    Pattern Question

--- src/PatternQstn.java
+++ src/PatternQstn.java
@@ -12,13 +12,35 @@ public class PatternQstn {
         objrtn.samenum();
         objrtn.decstar();
         objrtn.decnum();
+        objrtn.trianglestar();
+    }
+
+
+    public void trianglestar(){
+        System.out.println("Triangle Star");
+        int n=6;
+        for(int i=0; i<n; i++){
+            // For printing the spaces before stars in each row
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            // For printing the stars in each row
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            // For printing the spaces after the stars in each row
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public void decnum(){
         System.out.println("Decrease number");
-        for(int i=0; i<6; i++){
-            for(int j=i; j>6; j--){
-                System.out.print(6-j+1 + " ");
+        for(int i=6; i>0; i--){
+            for(int j=1; j<i; j++){
+                System.out.print(j +" ");
             }
             System.out.println();
         }
