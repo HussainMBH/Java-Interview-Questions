0d764370994a614ced6b177847a0a0bc09b45f92
HussainMBH <mbahirh624@gmail.com>
2023-11-01 15:09:57 +0530

    Day01

--- src/DayTry/Day01.java
+++ src/DayTry/Day01.java
@@ -9,13 +9,15 @@ public class Day01 {
         System.out.println("Day 01");
         Day01 objday01 = new Day01();
         objday01.Sortedarray();
-        for(int i=0; i< objday01.l; i++){
-            System.out.print(objday01.ar[i] + " ");
-        }
+        System.out.print(Arrays.toString(objday01.ar) + " ");
+        System.out.println();
+        objday01.Bubblesorted();
+        System.out.print(Arrays.toString(objday01.ar) + " ");
+
 
     }
     public void Sortedarray(){
-        System.out.println("Sorted Array");
+        System.out.println("Selection Sorted Array");
         for(int i=0; i<l-1; i++){
             int min = i;
             for (int j=i; j<l; j++){
@@ -27,6 +29,16 @@ public class Day01 {
             ar[min] = ar[i];
             ar[i] = tmp;
         }
-
+    }
+    public void Bubblesorted(){
+        for(int i=l-1; i>0; i--){
+            for (int j=0; j<i-1; j++){
+                if(ar[j]>ar[j+1]){
+                    int tmp = ar[j];
+                    ar[j] = ar[j+1];
+                    ar[j+1] = tmp;
+                }
+            }
+        }
     }
 }
