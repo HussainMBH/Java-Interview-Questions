8d501a82a4c389dadb84b72b9ea5f7a2deddb9cb
HussainMBH <mbahirh624@gmail.com>
2023-11-01 10:03:47 +0530

    Find Duplicates

--- src/try1.java
+++ src/try1.java
@@ -1,10 +1,19 @@
+import java.util.Arrays;
+
 public class try1 {
     public static void main(String[] args){
-        int arr[] = {1,2,3,4,5};
-        int rev = arr[0];
-        for(int i =arr.length -1; i<arr.length; i--){
-            rev = arr[i];
+        int ar[] = {1,2,2,3,4,5,6,6,7,8,9};
+        int n = -1;
+        int l = ar.length;
+        for(int i=0; i<l; i++){
+            if(ar[i]!= ar[i+1]){
+                System.out.println(ar[i]);
+            }
+            if(ar[i]==ar[i+1]){
+                System.out.println("It's duplicate "+ar[i]);
+            }
         }
-        System.out.println(rev);
+        System.out.println(Arrays.toString(ar));
+
     }
 }
