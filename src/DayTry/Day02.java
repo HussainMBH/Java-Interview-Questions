ef597e4b91074149b60218ad3d4c1597472b256d
Bahir Hussain <mbahirh624@gmail.com>
2023-11-13 19:46:57 +0530

    try

--- src/DayTry/Day02.java
+++ src/DayTry/Day02.java
@@ -1,5 +1,7 @@
 package DayTry;
 
+import java.util.Arrays;
+
 public class Day02 {
     public static void main(String[] args){
         int a[] = {1,2,3,5,6,7};
@@ -17,10 +19,46 @@ public class Day02 {
                 System.out.println("Find Missing Number: " + i);
             }
         }
-    }
 
-    public static int linearsearch(){
-        int a[] = {1,2,3,5,6,7};
+        int ar[] = {8,5,9,10,7,6,3,1,2,4};
+        int ll = ar.length;
+        for(int k=0; k<ll-1; k++){
+            int min = k;
+            for(int j=k; j<ll; j++){
+                if(ar[j]<ar[min]){
+                    min = j;
+                }
+            }
+            int tmp = ar[min];
+            ar[min] = ar[k];
+            ar[k] = tmp;
+        }
+        System.out.println("Sorted Array: "+Arrays.toString(ar));
 
+
+        int b[] = {0,1,1,2,3,3,3,5,4};
+        int cnt = 0;
+        int maxim = 0;
+        for(int i=0; i<b.length; i++){
+            if(b[i] == 3){
+                cnt++;
+            }
+            maxim = Math.max(maxim, cnt);
+        }
+        System.out.println(maxim);
+
+
+        int c[] = {10,7,9,8,3,5,1,2,4,6};
+        int cc = c.length;
+        for(int i=cc-1; i>=0; i--){
+            for(int j=0; j<=i-1; j++){
+                if(c[j]>c[j+1]){
+                    int tmp = c[j];
+                    c[j] = c[j+1];
+                    c[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(c));
     }
 }
---
+++ src/DayTry/Day03.java
@@ -0,0 +1,20 @@
+package DayTry;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day03 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int a[] = new int[6];
+        System.out.println("Enter numbers");
+
+        for (int i=0; i<a.length; i++){
+            int n = scn.nextInt();
+        }
+        System.out.println(Arrays.toString(a));
+
+
+    }
+}
