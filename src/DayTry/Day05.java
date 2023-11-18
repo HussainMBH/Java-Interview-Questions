b0686ec4c0ad9f538b8d607e0224ecc431012d0d
Bahir Hussain <mbahirh624@gmail.com>
2023-11-18 10:53:24 +0530

    Day05

--- src/DayTry/Day05.java
+++ src/DayTry/Day05.java
@@ -3,13 +3,18 @@ package DayTry;
 public class Day05 {
     public static void main(String[] args){
         int a[] = {5,9,7,6,8,3,5,7,5,8,1,4};
-        int l = a.length;
-        for(int i=0; i<l; i++){
-            for(int j=i+1; j<l; j++){
-                if(a[i] != a[i]){
+        System.out.println("Find Largest Element: " + findlargestnumber(a));
+    }
 
-                }
+    static int findlargestnumber(int a[]){
+
+        int l = a.length;
+        int max = a[0];
+        for(int i=0; i<l; i++){
+            if(a[i]>max){
+                max = a[i];
             }
         }
+        return max;
     }
 }
