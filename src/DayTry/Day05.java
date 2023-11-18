f4369274166b882bc618bfbff094d6b7a333e764
Bahir Hussain <mbahirh624@gmail.com>
2023-11-18 11:01:41 +0530

    day05

--- src/DayTry/Day05.java
+++ src/DayTry/Day05.java
@@ -1,9 +1,12 @@
 package DayTry;
 
+import java.util.Arrays;
+
 public class Day05 {
     public static void main(String[] args){
         int a[] = {5,9,7,6,8,3,5,7,5,8,1,4};
-        System.out.println("Find Largest Element: " + findlargestnumber(a));
+        System.out.println("Find Largest Element by using loop: " + findlargestnumber(a));
+        System.out.println("Find Largest Element by using sorting: " + findlargestbysort(a));
     }
 
     static int findlargestnumber(int a[]){
@@ -16,5 +19,11 @@ public class Day05 {
             }
         }
         return max;
+    }
+
+    static int findlargestbysort(int a[]){
+        int l = a.length;
+        Arrays.sort(a);
+        return a[l-1];
     }
 }
