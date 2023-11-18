94f03a8ea04542c5c21475b6b9dd8bc7d7952a6d
Bahir Hussain <mbahirh624@gmail.com>
2023-11-18 11:51:31 +0530

    Secondlarge

--- src/DayTry/Day05.java
+++ src/DayTry/Day05.java
@@ -7,6 +7,7 @@ public class Day05 {
         int a[] = {5,9,7,6,8,3,5,7,5,8,1,4};
         System.out.println("Find Largest Element by using loop: " + findlargestnumber(a));
         System.out.println("Find Largest Element by using sorting: " + findlargestbysort(a));
+        System.out.println(seconlargestelement(a));
     }
 
     static int findlargestnumber(int a[]){
@@ -25,5 +26,20 @@ public class Day05 {
         int l = a.length;
         Arrays.sort(a);
         return a[l-1];
+    }
+
+    static int seconlargestelement(int a[]){
+        int l = a.length;
+        int maxi = a[0];
+        int scnflargest = Integer.MIN_VALUE;
+        for(int i=0; i<l; i++){
+           maxi = Math.max(maxi, a[i]);
+        }
+        for (int i=0; i<l; i++){
+            if(scnflargest< a[i] && a[i] != maxi){
+                scnflargest = a[i];
+            }
+        }
+        return scnflargest;
     }
 }
