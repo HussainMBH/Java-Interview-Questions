ab56330d3a9558e1b4d70b960067d31107b18832
Bahir Hussain <mbahirh624@gmail.com>
2023-11-21 20:24:51 +0530

    Array List

--- src/ArrayListQstn.java
+++ src/ArrayListQstn.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class ArrayListQstn {
     public static void main(String[] args){
-
+        ArrayList<Integer> alist = new ArrayList<Integer>(); //Flexible for adding many elements
+        for(int i= 0; i<=10; i++){
+            alist.add(i);
+        }
+        System.out.println(alist);
     }
 }
