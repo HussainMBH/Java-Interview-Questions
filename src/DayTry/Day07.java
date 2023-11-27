cd99cc3fef848b92c151194009fe325b86e89103
Bahir Hussain <mbahirh624@gmail.com>
2023-11-27 13:09:41 +0530

    Update

--- src/DayTry/Day07.java
+++ src/DayTry/Day07.java
@@ -16,10 +16,23 @@ public class Day07 {
             }
         }
 
-//        System.out.println();
-//        Day07 obj = new Day07();
-//        obj.hashsettry();
+            System.out.println();
+            Day07 obj = new Day07();
+            obj.hashsettry();
     }
 
-
+    void hashsettry(){
+        int a[] = {1,2,2,3,3,3,4,5,6,4,2,1,8,9,7,10,9};
+        HashSet<Integer> nwhst = new HashSet<>();
+        for(int i=0; i<a.length; i++){
+            nwhst.add(a[i]);
+        }
+        System.out.println(nwhst);
+        HashSet<Integer> hst = new HashSet<>();
+        hst.add(10);
+        hst.add(10);
+        hst.add(11);
+        hst.add(15);
+        System.out.println(hst);
+    }
 }
