18eac1ba9007b2e65039b193c0e07b117a972c75
Bahir Hussain <mbahirh624@gmail.com>
2023-11-28 11:10:33 +0530

    Day09

--- src/DayTry/Day08.java
+++ src/DayTry/Day08.java
@@ -7,6 +7,7 @@ public class Day08 {
     public static void main(String[] args){
         Day08 objday8 = new Day08();
         objday8.selectionsorting();
+        objday8.bublesorting();
 
     }
 
@@ -23,6 +24,21 @@ public class Day08 {
             int tmp = a[min];
             a[min] = a[i];
             a[i] = tmp;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+
+    void bublesorting(){
+        int a[] = {9,5,6,7,3,1,4,2,10,8};
+        int l = a.length;
+        for(int i = l-1; i>=0; i--){
+            for(int j=0; j<=i-1; j++){
+                if(a[j]>a[j+1]){
+                    int tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                }
+            }
         }
         System.out.println(Arrays.toString(a));
     }
---
+++ src/DayTry/Day09.java
@@ -0,0 +1,25 @@
+package DayTry;
+
+import java.util.Scanner;
+
+public class Day09 {
+    public static void main(String[] args){
+        Day09 objday9 = new Day09();
+        objday9.substring();
+    }
+
+    void substring(){
+        String a = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("FInd SubString: ");
+        a = a.substring(5, 16);
+        System.out.println(a);
+        String aa = "abcdefghijklmnopqrstuvwxyz";
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your starting and ending point");
+        int starting = scn.nextInt();
+        int ending = scn.nextInt();
+        for(int i=starting; i<ending; i++){
+            System.out.print(aa.charAt(i) + " ");
+        }
+    }
+}
