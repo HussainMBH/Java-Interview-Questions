5a38dc08cba798d278be69610e0107db03a42e72
Bahir Hussain <mbahirh624@gmail.com>
2023-11-29 13:30:58 +0530

    Strip Trailing

--- src/DayTry/Day10.java
+++ src/DayTry/Day10.java
@@ -8,8 +8,34 @@ public class Day10 {
         Day10 objday10 = new Day10();
         objday10.findduplicate();
         objday10.removeduplicate();
+        objday10.striptrailing();
     }
 
+    void striptrailing(){
+        String s = "Mohamed Bahir Hussain           ";
+        int l = s.length();
+        System.out.println(l);
+        s = s.stripTrailing();
+        System.out.println(s);
+        System.out.println(s.length());
+        int end = 0;
+        int count = 0;
+        for(int i=s.length()-1; i>=0; i++){
+            if(s.charAt(i) == ' '){
+                continue;
+            }
+            else {
+                end = i;
+                break;
+            }
+        }
+        for(int i=0; i<=end; i++){
+            System.out.print(s.charAt(i));
+            count++;
+        }
+        System.out.println();
+        System.out.println(count);
+    }
     void removeduplicate(){
         int a[] = {9,5,4,7,10,3,4,6,2,5,7,1,8};
         int l = a.length;
