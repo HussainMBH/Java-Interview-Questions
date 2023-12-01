1bde14755af537f6d6f16d4359ab606c20313d06
Bahir Hussain <mbahirh624@gmail.com>
2023-12-01 11:08:46 +0530

    Day12

--- src/DayTry/Day12.java
+++ src/DayTry/Day12.java
@@ -1,12 +1,27 @@
 package DayTry;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Day12 {
     public static void main(String[] args) {
         Day12 objday12 = new Day12();
         objday12.linearsearching();
         objday12.bubblesorting();
+        objday12.removeduplicate();
+    }
+
+    void removeduplicate(){
+        int a[] = {8,3,7,9,10,5,8,6,2,2,1,4};
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i=0; i<=a.length-1; i++){
+            for (int j=1; j<a.length; j++){
+                if(a[i] != a[j]){
+                    hs.add(a[i]);
+                }
+            }
+        }
+        System.out.println(hs);
     }
 
     void bubblesorting(){
