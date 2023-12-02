e9b5498c829591b71bbd69bcfe450484025e55f3
Bahir Hussain <mbahirh624@gmail.com>
2023-12-02 18:40:52 +0530

    Day13

--- src/DayTry/Day13.java
+++ src/DayTry/Day13.java
@@ -4,6 +4,24 @@ public class Day13 {
     public static void main(String[] args) {
         Day13 obj13 = new Day13();
         obj13.striptrailing();
+        obj13.linearsearch();
+    }
+
+    void linearsearch(){
+        String s = "Hello Mohamed Bahir Hussain";
+        char key = 'a';
+        int flag = 0;
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == key){
+                count++;
+                flag = 1;
+            }
+        }
+        if(flag == 0) {
+            System.out.println("Sorry your element is not find in here");
+        }
+        System.out.println(count);
     }
 
     void striptrailing(){
