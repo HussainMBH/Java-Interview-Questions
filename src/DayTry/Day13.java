2c1f57ab0c4025ae29ede4b15dc35e72f4c4de23
Bahir Hussain <mbahirh624@gmail.com>
2023-12-02 18:35:19 +0530

    Day13

--- src/DayTry/Day13.java
+++ src/DayTry/Day13.java
@@ -2,6 +2,29 @@ package DayTry;
 
 public class Day13 {
     public static void main(String[] args) {
+        Day13 obj13 = new Day13();
+        obj13.striptrailing();
+    }
 
+    void striptrailing(){
+        String s = "Hello Mohamed Bahir Hussain        ";
+        System.out.println(s.length());
+        int end = 0;
+        int count = 0;
+        for(int i=s.length()-1; i>= 0; i--){
+            if(s.charAt(i) == ' '){
+                continue;
+            }
+            else {
+                end = i;
+                break;
+            }
+        }
+        for (int i=0; i<=end; i++){
+            System.out.print(s.charAt(i));
+            count ++;
+        }
+        System.out.println();
+        System.out.println(count);
     }
 }
