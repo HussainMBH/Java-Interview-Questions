bee26a738020366ed4a2e673516270b431dc0600
Bahir Hussain <mbahirh624@gmail.com>
2023-12-07 10:59:10 +0530

    Day17

--- src/DayTry/Day17.java
+++ src/DayTry/Day17.java
@@ -6,6 +6,21 @@ public class Day17 {
     public static void main(String[] args) {
         Day17 obj = new Day17();
         obj.sortingarray();
+        obj.findlargest3element();
+    }
+
+    void findlargest3element(){
+        int a[] = {8,9,7,6,4,5,3,1,2,10};
+        System.out.println("First method");
+        Arrays.sort(a);
+        for(int i= a.length-1; i> a.length-4; i--){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Second method");
+        System.out.print(a[a.length-1] + " ");
+        System.out.print(a[a.length-2] + " ");
+        System.out.print(a[a.length-3]);
     }
 
     void sortingarray(){
