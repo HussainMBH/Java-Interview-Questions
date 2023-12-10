784883e78469a99e38ce626d77a08b38012c8487
Bahir Hussain <mbahirh624@gmail.com>
2023-12-10 11:13:23 +0530

    Day20

--- src/DayTry/Day20.java
+++ src/DayTry/Day20.java
@@ -4,6 +4,18 @@ public class Day20 {
     public static void main(String[] args) {
         Day20 obj = new Day20();
         obj.binarysearch();
+        obj.countsoneelement();
+    }
+
+    void countsoneelement(){
+        int a[] = {5,2,4,2,8,7,3,2,2,6,9,2,7,2,1,4,5};
+        int count =0;
+        for(int i=0; i<a.length; i++){
+            if(a[i] == 2){
+                count++;
+            }
+        }
+        System.out.println("Total number two is " +count);
     }
     void binarysearch(){
         int a[] = {8,10,4,1,5,7,6,2,3,9};
