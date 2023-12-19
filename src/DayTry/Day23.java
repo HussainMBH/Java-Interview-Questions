296e1e7d8da39495c98707bcab30f94f789b3a3f
Bahir Hussain <mbahirh624@gmail.com>
2023-12-19 18:40:44 +0530

    Day23

--- src/DayTry/Day23.java
+++ src/DayTry/Day23.java
@@ -1,10 +1,28 @@
 package DayTry;
 
+import java.util.Arrays;
+
 public class Day23 {
     public static void main(String[] args) {
         Day23 obj = new Day23();
         obj.findsmallandbiggest();
         obj.scndlargestandsmallest();
+        obj.bubblesorting();
+    }
+
+    void bubblesorting(){
+        int a[] = {10,2,55,71,5,3,83,95,63,41,8, 125, 1,-2,-4};
+        for(int i=a.length-1; i>=0; i--){
+            for(int j=0; j<i; j++){
+                if(a[j]>a[j+1]){
+                    int tmp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(a));
     }
 
     void scndlargestandsmallest(){
