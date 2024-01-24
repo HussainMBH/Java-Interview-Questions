c350fbd03123b4984562c7e7780a51bff60c68cf
Bahir Hussain <mbahirh624@gmail.com>
2024-01-24 18:14:46 +0530

    Day31

--- src/DayTry/Day30.java
+++ src/DayTry/Day30.java
@@ -8,6 +8,16 @@ public class Day30 {
         removeduplicate();
     }
 
+    static void findduplicate(){
+        int a[] = {5,7,6,4,3,9,8,1,6,4,7,8,10,2,5,7,3,1,10,5,8,9};
+        System.out.println(Arrays.toString(a));
+        for(int i=0; i<a.length; i++){
+            for(int j=i+1; j<a.length; j++){
+
+            }
+        }
+    }
+
     static void removeduplicate(){
         int a[] = {5,7,6,4,3,9,8,1,6,4,7,8,10,2,5,7,3,1,10,5,8,9};
         System.out.println(Arrays.toString(a));
---
+++ src/DayTry/Day31.java
@@ -0,0 +1,45 @@
+package DayTry;
+
+import java.util.Arrays;
+
+public class Day31 {
+    public static void main(String[] args) {
+        removeduplicate();
+        System.out.println();
+        findduplicate();
+    }
+
+    static void findduplicate(){
+        int a[] = {5,7,6,4,3,9,8,1,4};
+        System.out.println(Arrays.toString(a));
+        int flag = 0;
+        for(int i=0; i<a.length; i++){
+            for(int j=i+1; j<a.length; j++){
+                if(a[i] == a[j]){
+                    System.out.println("Element " + a[i] + " has duplicate element in this array");
+                    flag = 1;
+                }
+            }
+        }
+        if(flag == 0){
+            System.out.println("no duplicate found here");
+        }
+    }
+
+    static void removeduplicate(){
+        int a[] = {5,7,6,4,3,9,8,1,6,4,7,8,10,2,5,7,3,1,10,5,8,9};
+        System.out.println(Arrays.toString(a));
+        int flag = -1;
+        for(int i=0; i<a.length; i++){
+            if(a[i] != -1){
+                for(int j=i+1; j<a.length; j++){
+                    if(a[i] == a[j]){
+                        a[j] = flag;
+                    }
+                }
+                System.out.print(a[i] + " ");
+            }
+
+        }
+    }
+}
