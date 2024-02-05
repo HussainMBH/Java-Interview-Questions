aeae75ab117276f08b88d5bccd174b4844fcd4cf
Bahir Hussain <mbahirh624@gmail.com>
2024-02-05 19:22:46 +0530

    Update

--- src/DayTry/Day33.java
+++ src/DayTry/Day33.java
@@ -4,14 +4,31 @@ import java.util.Arrays;
 
 public class Day33 {
     public static void main(String[] args) {
+        removeduplicate();
+        System.out.println();
         checkarray();
     }
 
+    static void removeduplicate(){
+        int a[] = {3,5,7,1,6,5};
+        int flag = -1;
+        for (int i=0; i<a.length-1; i++){
+            if(flag != a[i]){
+                for(int j=i+1; j<a.length; j++){
+                    if(a[i] == a[j]){
+                        a[j] = flag;
+                    }
+                }
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+
     static void checkarray(){
-        int a[] = {1,2,3};
-        int ab[] = {5,8,3,4,9,3,10,7,2,6,1};
+        int ab[] = {1,2,3};
+        int a[] = {5,8,3,4,9,3,10,7,2,6};
         Arrays.sort(ab);
-        System.out.println(ab);
+        System.out.println(Arrays.toString(ab));
         int flag = 0;
         for(int i=0; i<a.length-1; i++){
             if(a[i]<a[i+1]){
