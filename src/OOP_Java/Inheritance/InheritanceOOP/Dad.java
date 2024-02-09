431dbb79ccd4fa12c38b3ec7c0550fc9e51e69b8
Bahir Hussain <mbahirh624@gmail.com>
2024-02-09 08:41:52 +0530

    Inheritance

---
+++ src/OOP_Java/Inheritance/InheritanceOOP/Dad.java
@@ -0,0 +1,16 @@
+package OOP_Java.Inheritance.InheritanceOOP;
+
+public class Dad {
+    public static void main(String[] args) {
+        System.out.println("Hello i'm john's dad");
+    }
+
+    static void money(){
+        System.out.println("i have $10M");
+        System.out.println("I have bank account in Swiss Bank");
+    }
+
+    static void bike(){
+        System.out.println("I have FZ V3.0");
+    }
+}
