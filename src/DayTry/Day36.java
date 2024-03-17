95702623fe1c902cbf31ebf53b0c7369b49854f3
Bahir Hussain <mbahirh624@gmail.com>
2024-03-17 20:46:03 +0530

    Update

--- src/DayTry/Day36.java
+++ src/DayTry/Day36.java
@@ -6,12 +6,19 @@ public class Day36 {
     }
 
     static void linearsearch(){
-        int key = 6;
+        int key = 16;
         int a [] = {4,7,3,5,6,9,1,3,2};
+        int flag = 0;
         for(int i=0; i<a.length; i++){
-            if(a[i] == key){
-                System.out.println("Element " + key + " found at " + i + "th place.");
+            if(flag == 0) {
+                if (a[i] == key) {
+                    System.out.println("Element " + key + " found at " + i + "th place.");
+                    flag = 1;
+                }
             }
+        }
+        if(flag == 0){
+            System.out.println("Element " + key + " not found at here");
         }
     }
 }
